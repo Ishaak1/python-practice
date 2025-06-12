4e3de9d1a72ad19fa9911502f7694828d9f54317
Ishaak <Ishaak019@gmail.com>
2025-06-12 09:41:47 +0200

    implemented calculator function

--- calculator.py
+++ calculator.py
@@ -1,8 +1,32 @@
 
-def calculator():
 
-	result = 0
-	return result
+def calculator(number1, number2, symbol):
+	
+	if symbol == "+":
 
-x = calculator()
-print(x)
+		answer = number1 + number2
+
+	elif symbol == "-":
+
+		answer = number1 - number2
+
+	elif symbol == "*":
+
+		answer = number1 * number2
+
+	elif symbol == "/":
+
+		answer = number1 / number2
+
+	elif symbol == "**":
+
+		answer = number1 ** number2
+
+	return answer
+
+number1 = "2"
+number2 = "3.2"
+symbol = "*"
+the_answer = calculator(number1, number2, symbol)
+print(the_answer)
+
